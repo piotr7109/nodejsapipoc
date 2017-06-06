e3b752585c0af223c17a187b680ce7931e4b0717
Fix <piotr.i.fic@hsbc.com>
2017-06-06 11:54:50 +0200

    project structure & basic gulp config

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    shell = require('gulp-shell'),
+    webpack = require('webpack'),
+    webpackConfig = require('./webpack.config.js');
+
+gulp.task('watch', () => gulp.watch(['/src/app/**/*.{js,jsx}'], ['build-js']));
+
+gulp.task('build', () => gulp.start('build-js'));
+
+gulp.task('default', ['watch']);
+
+gulp.task('build-js', callback => {
+    webpack(webpackConfig, (err, stats) => runWebpackConfig(err, stats, callback));
+});
+
+function runWebpackConfig(err, stats, callback) {
+    if (err) {
+        throw new gutil.PluginError('webpack', err);
+    }
+    gutil.log('[webpack] Completed\n' + stats.toString({
+            assets: true,
+            chunks: false,
+            chunkModules: false,
+            colors: true,
+            hash: false,
+            timings: false,
+            version: false
+        }));
+    callback();
+}
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack'),
+    happypack = require('happypack'),
+    path = require('path'),
+    APP_DIR = path.resolve(__dirname, 'src/app');
+    BUILD_DIR = path.resolve(__dirname, 'src/target');
+
+module.exports = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel'
+            }
+        ]
+    }
+};
