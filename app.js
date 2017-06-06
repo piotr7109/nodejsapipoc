f4bbb97b58bb769b3d8c8f9cccc36a5c249f48d2
44002771 <mikael.kocharyan@hsbc.com>
2017-06-06 12:40:57 +0200

    app.js levelup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express'),
 	fs = require('fs'),
+	path = require('path'),
 	app = express();
+
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function(req, res){
 	fs.readFile('index.html', function(error, content){
