5e4f6d7442f2178d6605b912439f13bba709ad9a
Fix <piotr.i.fic@hsbc.com>
2017-06-12 12:40:57 +0200

    form styling & states update

--- src/app/pages/Login.jsx
+++ src/app/pages/Login.jsx
@@ -3,10 +3,12 @@ import serialize from 'form-serialize';
 import * as axios from 'axios';
 
 export default class Login extends React.Component {
-
-    constructor() {
-        super();
+    componentWillMount() {
+        window.localStorage.clear();//TO REMOVE
         this.state = {mode: 0};
+        if (localStorage.getItem('userId') !== null) {
+            this.updateMode(3);
+        }
     }
 
     updateMode(mode) {
@@ -17,17 +19,23 @@ export default class Login extends React.Component {
         event.preventDefault();
         axios
             .post('/user/login', this.getFormData(event))
-            .then(result => console.log(result));
+            .then(result => {
+                const userId = result.data.id;
+
+                if (userId) {
+                    localStorage.setItem('userId', userId);
+                    return 1;
+                }
+
+                return -1;
+            })
+            .then(mode => this.updateMode(mode));
     }
 
     getFormData(event) {
-        const target = event.target,
-            data = serialize(target, {hash: true}),
-            formData:FormData = new FormData();
+        const target = event.target;
 
-        formData.append('userData', JSON.stringify(data));
-
-        return formData;
+        return serialize(target, {hash: true});
     }
 
     getForm() {
@@ -54,13 +62,16 @@ export default class Login extends React.Component {
                 return this.getForm();
                 break;
             case 1:
-                return <div>mode 1</div>;
+                return <div className="alert alert-success">Succesfully logged in!</div>;
                 break;
             case 2:
                 return <div>Success</div>;
                 break;
+            case 3:
+                return <div className="alert alert-warning">You are already logged in!</div>;
+                break;
             case -1:
-                return <div>Fail</div>;
+                return <div className="alert alert-danger">Enter a valid credentials</div>;
                 break;
             case -10:
                 return <div>Loading</div>;
