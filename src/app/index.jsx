bd70cad0819624ab38d5861502d1b68d851c0cb2
Fix <piotr.i.fic@hsbc.com>
2017-06-12 08:19:51 +0200

    test

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -13,6 +13,7 @@ class App extends React.Component {
                     <Route path="/" component={Home}/>
                     <Route path="/login" component={Login}/>
                 </Route>
+
             </Router>
         );
     }
